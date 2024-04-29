34188fe76d3a012ea5a8e5a9bae22b459d1a0c08
Mateusz Biel <matbiel@gmail.com>
2024-04-29 14:32:57 +0200

    Enable cross zone

--- alb_vs_nlb/load_balancer.tf
+++ alb_vs_nlb/load_balancer.tf
@@ -1,9 +1,10 @@
 resource "aws_lb" "application_load_balancer" {
-  name               = "application-load-balancer"
-  load_balancer_type = "application"
-  internal           = false
-  security_groups    = [aws_security_group.load_balancer_security_group.id]
-  subnets            = aws_subnet.public_subnet[*].id
+  name                             = "application-load-balancer"
+  load_balancer_type               = "application"
+  internal                         = false
+  security_groups                  = [aws_security_group.load_balancer_security_group.id]
+  subnets                          = aws_subnet.public_subnet[*].id
+  enable_cross_zone_load_balancing = true
 }
 
 resource "aws_lb_target_group" "application_lb_target_group" {
@@ -24,11 +25,12 @@ resource "aws_lb_listener" "application_lb_listener" {
 }
 
 resource "aws_lb" "network_load_balancer" {
-  name               = "network-load-balancer"
-  load_balancer_type = "network"
-  internal           = false
-  security_groups    = [aws_security_group.load_balancer_security_group.id]
-  subnets            = aws_subnet.public_subnet[*].id
+  name                             = "network-load-balancer"
+  load_balancer_type               = "network"
+  internal                         = false
+  security_groups                  = [aws_security_group.load_balancer_security_group.id]
+  subnets                          = aws_subnet.public_subnet[*].id
+  enable_cross_zone_load_balancing = true
 }
 
 resource "aws_lb_target_group" "network_lb_target_group" {

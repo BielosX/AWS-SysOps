d7ae1437bc6385b68a3491aacecf14ed23711643
Mateusz Biel <matbiel@gmail.com>
2024-04-30 12:52:59 +0200

    Format python file

--- alb_vs_nlb/tester.py
+++ alb_vs_nlb/tester.py
@@ -1,20 +1,22 @@
 from http.client import HTTPConnection
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Tests AWS LB connections.')
-    parser.add_argument('domain')
-    parser.add_argument('-c', '--count', default=10)
-    parser.add_argument('-p', '--port', default=80)
+    parser = argparse.ArgumentParser(description="Tests AWS LB connections.")
+    parser.add_argument("domain")
+    parser.add_argument("-c", "--count", default=10)
+    parser.add_argument("-p", "--port", default=80)
     args = parser.parse_args()
 
     connection = HTTPConnection(args.domain, args.port)
     for _ in range(0, args.count):
-        connection.request('GET', "")
+        connection.request("GET", "")
         response = connection.getresponse()
         response_body = response.read()
         print(response.status, response_body)
     connection.close()
+
 
 if __name__ == "__main__":
     main()
